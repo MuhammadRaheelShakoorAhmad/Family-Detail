0fcb08119fecc90179321ea05f02c34a6f4138a7
Raheel <raheelrana148@gmail.com>
2022-06-14 11:42:12 +0100

    Update: Overridet the create method so, you can easily implemenet the price ranges

--- myApp/apis/viewSets.py
+++ myApp/apis/viewSets.py
@@ -2,6 +2,8 @@ from rest_framework.viewsets import ModelViewSet
 from myApp.models import Offer, FamilyType, ProgressState, Family
 from rest_framework.permissions import AllowAny
 from myApp.apis.serializers import OfferSerializer, FamilyTypeSerializer, PossibleProgressStateSerializer, FamilySerializer
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class OfferViewSet(ModelViewSet):
@@ -24,4 +26,12 @@ class FamilyViewSet(ModelViewSet):
     queryset = Family.objects.all()
     permission_classes = [AllowAny, ]
     serializer_class = FamilySerializer
+
+    def create(self, request, *args, **kwargs):
+        """You can implement the price ranges here according to your logic"""
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
